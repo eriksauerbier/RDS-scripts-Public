c440fafbf15f7c2f672479ed5c3f48d99b3a1b9a
eriksauerbier <erik.sauerbier@mailbox.org>
2024-12-09 16:48:14 +0100

    Create Set-WTS-Cert.ps1

---
+++ Set-WTS-Cert.ps1
@@ -0,0 +1,10 @@
+$certs=get-childitem -path cert:/LocalMachine/My
+foreach ($c in $certs) {   
+    if ($c.Subject.IndexOf("CN=*.") -eq 0) {
+        write-host -ForegroundColor Yellow  $c.Subject
+        $t=[string]$c.thumbprint
+		Set-WmiInstance -Path ((Get-WmiObject -class "Win32_TSGeneralSetting" -Namespace root\cimv2\terminalservices -Filter "TerminalName='RDP-tcp'").__path) -argument @{SSLCertificateSHA1Hash=$t
+        # WMIC ist ab Server 2025 nicht mehr funktional, da abgekuendigt
+		#wmic /namespace:\\root\CIMV2\TerminalServices PATH Win32_TSGeneralSetting Set SSLCertificateSHA1Hash=$t 
+    } 
+}
